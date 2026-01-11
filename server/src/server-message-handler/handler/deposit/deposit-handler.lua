164fecf6155d10dd61d319535ddc6533ca6246b6
Zamralik <vuk.kulvar+github@gmail.com>
2026-01-11 14:00:58 +0100

    feat: user friendly bag index for depositing

--- server/src/server-message-handler/handler/deposit/deposit-handler.lua
+++ server/src/server-message-handler/handler/deposit/deposit-handler.lua
@@ -6,8 +6,10 @@ local getInventoryBagID = require("utility.get-inventory-bag-id")
 local DepositHandler = AbstractHandler:extend()
 
 function DepositHandler:execute(player, data)
-	local bag_id = data[1] or nil
+	local bag_index = data[1] or nil
 	local slot_id = data[2] or nil
+
+	local bag_id = getInventoryBagID(bag_index)
 
 	if bag_id == nil or slot_id == nil then
 		PrintError("DepositHandler: Invalid bag_index or slot_id provided.")
@@ -16,6 +18,13 @@ function DepositHandler:execute(player, data)
 	end
 
 	local item = player:GetItemByPos(bag_id, slot_id)
+
+	if item == nil then
+		PrintError("DepositHandler: No item found in bag "..tostring(bag_index).." slot "..tostring(slot_id)..".")
+
+		return
+	end
+
 	local item_template = item:GetItemTemplate()
 	local updated_quantity = Storage:depositItem(player, item)
 	local item_template_id = item_template:GetItemId()
