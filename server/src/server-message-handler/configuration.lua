a929125a438f2011b3d6ff46b5d80b8d24199b4f
Zamralik <vuk.kulvar+github@gmail.com>
2026-01-11 14:00:24 +0100

    chore: server code indent with tabs

--- server/src/server-message-handler/configuration.lua
+++ server/src/server-message-handler/configuration.lua
@@ -5,37 +5,37 @@ local DepositHandler = require("server-message-handler.handler.deposit/deposit-h
 local WithdrawHandler = require("server-message-handler.handler.withdraw/withdraw-handler")
 
 function ListAll(player)
-    local handler = ListAllHandler:new()
+	local handler = ListAllHandler:new()
 
-    return handler:execute(player)
+	return handler:execute(player)
 end
 
 function DepositAll(player)
-    local handler = DepositAllHandler:new()
+	local handler = DepositAllHandler:new()
 
-    handler:execute(player)
+	handler:execute(player)
 end
 
 function Deposit(player, bag_id, slot_id)
-    local handler = DepositHandler:new()
+	local handler = DepositHandler:new()
 
-    handler:execute(player, bag_id, slot_id)
+	handler:execute(player, bag_id, slot_id)
 end
 
 function Widthdraw(player, item_template_id)
-    local handler = WithdrawHandler:new()
+	local handler = WithdrawHandler:new()
 
-    handler:execute(player, item_template_id)
+	handler:execute(player, item_template_id)
 end
 
 local configuration = {
-    Prefix = "ItemVault",
-    Functions = {
-        [ServerResponseEnum.LIST_ALL] = "ListAll",
-        [ServerResponseEnum.DEPOSIT_ALL] = "DepositAll",
-        [ServerResponseEnum.DEPOSIT] = "Deposit",
-        [ServerResponseEnum.WITHDRAW] = "Widthdraw"
-    }
+	Prefix = "ItemVault",
+	Functions = {
+		[ServerResponseEnum.LIST_ALL] = "ListAll",
+		[ServerResponseEnum.DEPOSIT_ALL] = "DepositAll",
+		[ServerResponseEnum.DEPOSIT] = "Deposit",
+		[ServerResponseEnum.WITHDRAW] = "Widthdraw"
+	}
 }
 
 
--- server/src/server-message-handler/handler/deposit/deposit-handler.lua
+++ server/src/server-message-handler/handler/deposit/deposit-handler.lua
@@ -1,25 +1,26 @@
 local AbstractHandler = require("server-message-handler.handler.abstract.abstract-handler")
 local ClientHandlerEnum = require("server-message-handler.definition.enum.client-handler-enum")
 local Storage = require("storage.storage")
+local getInventoryBagID = require("utility.get-inventory-bag-id")
 
 local DepositHandler = AbstractHandler:extend()
 
 function DepositHandler:execute(player, data)
-    local bag_id = data[1] or nil
-    local slot_id = data[2] or nil
+	local bag_id = data[1] or nil
+	local slot_id = data[2] or nil
 
-    if bag_id == nil or slot_id == nil then
-        PrintError("DepositHandler: Invalid bag_id or slot_id provided.")
+	if bag_id == nil or slot_id == nil then
+		PrintError("DepositHandler: Invalid bag_index or slot_id provided.")
 
-        return
-    end
+		return
+	end
 
-    local item = player:GetItemByPos(bag_id, slot_id)
-    local item_template = item:GetItemTemplate()
-    local updated_quantity = Storage:depositItem(player, item)
-    local item_template_id = item_template:GetItemId()
+	local item = player:GetItemByPos(bag_id, slot_id)
+	local item_template = item:GetItemTemplate()
+	local updated_quantity = Storage:depositItem(player, item)
+	local item_template_id = item_template:GetItemId()
 
-    player:SendServerResponse("ItemVault", ClientHandlerEnum.DEPOSIT_RESPONSE, item_template_id, updated_quantity)
+	player:SendServerResponse("ItemVault", ClientHandlerEnum.DEPOSIT_RESPONSE, item_template_id, updated_quantity)
 end
 
 return DepositHandler
