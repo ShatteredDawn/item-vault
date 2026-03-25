643bfe69a04119b0c0bad6a84945e88181c3dd63
Zamralik <vuk.kulvar+github@gmail.com>
2026-03-25 13:07:15 +0100

    feat: add Zul Gurub currencies (#13)

---
+++ client/src/items/zul-gurub.lua
@@ -0,0 +1,112 @@
+local _, internal = ...
+
+internal.appendData({
+	{
+		id = 19698,
+		name = "Zulian Coin",
+		icon = "Interface\\Icons\\INV_Misc_ArmorKit_11",
+		categories = { 0 },
+	},
+	{
+		id = 19699,
+		name = "Razzashi Coin",
+		icon = "Interface\\Icons\\INV_Misc_Coin_10",
+		categories = { 0 },
+	},
+	{
+		id = 19700,
+		name = "Hakkari Coin",
+		icon = "Interface\\Icons\\INV_Misc_Coin_09",
+		categories = { 0 },
+	},
+	{
+		id = 19701,
+		name = "Gurubashi Coin",
+		icon = "Interface\\Icons\\INV_Misc_Coin_07",
+		categories = { 0 },
+	},
+	{
+		id = 19702,
+		name = "Vilebranch Coin",
+		icon = "Interface\\Icons\\INV_Misc_Coin_13",
+		categories = { 0 },
+	},
+	{
+		id = 19703,
+		name = "Witherbark Coin",
+		icon = "Interface\\Icons\\INV_Misc_Coin_14",
+		categories = { 0 },
+	},
+	{
+		id = 19704,
+		name = "Sandfury Coin",
+		icon = "Interface\\Icons\\INV_Misc_Coin_15",
+		categories = { 0 },
+	},
+	{
+		id = 19705,
+		name = "Skullsplitter Coin",
+		icon = "Interface\\Icons\\INV_Misc_Coin_12",
+		categories = { 0 },
+	},
+	{
+		id = 19706,
+		name = "Bloodscalp Coin",
+		icon = "Interface\\Icons\\INV_Misc_Coin_11",
+		categories = { 0 },
+	},
+	{
+		id = 19707,
+		name = "Red Hakkari Bijou",
+		icon = "Interface\\Icons\\INV_Bijou_Red",
+		categories = { 0 },
+	},
+	{
+		id = 19708,
+		name = "Blue Hakkari Bijou",
+		icon = "Interface\\Icons\\INV_Bijou_Blue",
+		categories = { 0 },
+	},
+	{
+		id = 19709,
+		name = "Yellow Hakkari Bijou",
+		icon = "Interface\\Icons\\INV_Bijou_Yellow",
+		categories = { 0 },
+	},
+	{
+		id = 19710,
+		name = "Orange Hakkari Bijou",
+		icon = "Interface\\Icons\\INV_Bijou_Orange",
+		categories = { 0 },
+	},
+	{
+		id = 19711,
+		name = "Green Hakkari Bijou",
+		icon = "Interface\\Icons\\INV_Bijou_Green",
+		categories = { 0 },
+	},
+	{
+		id = 19712,
+		name = "Purple Hakkari Bijou",
+		icon = "Interface\\Icons\\INV_Bijou_Purple",
+		categories = { 0 },
+	},
+	{
+		id = 19713,
+		name = "Bronze Hakkari Bijou",
+		icon = "Interface\\Icons\\INV_Bijou_Bronze",
+		categories = { 0 },
+	},
+	{
+		id = 19714,
+		name = "Silver Hakkari Bijou",
+		icon = "Interface\\Icons\\INV_Bijou_Silver",
+		categories = { 0 },
+	},
+	{
+		id = 19715,
+		name = "Gold Hakkari Bijou",
+		icon = "Interface\\Icons\\INV_Bijou_Gold",
+		categories = { 0 },
+	},
+});
